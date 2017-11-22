2abb91509d10dd7bc857987d094e9bfc349f8317
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2017-11-22 06:14:28 +0200

    Switch to discord.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const auth = require('./auth.json');
 
-const bot = new Discord.Client({
-    token: auth.token,
-    autorun: true,
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('Logged in!');
 });
 
-bot.on('ready', function() {
-    console.log('Logged in as %s - %s\n', bot.username, bot.id);
-});
-
-bot.on('message', function(user, userID, channelID, message, event) {
-    if (message === 'ping') {
-        bot.sendMessage({
-            to: channelID,
-            message: 'pong',
-        });
+bot.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
     }
 });
+
+bot.login(auth.token);
