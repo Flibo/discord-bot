90271e691d84f0d248e01d9586479c8bac12553e
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2017-12-01 03:19:43 +0200

    Add folder for all scripts

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -9,7 +9,7 @@ dockernetwork="discordbot_default"
 >&2 echo "Attempting to initialize DB"
 
 # Initialize schema with sqitch
-cd $(dirname "$BASH_SOURCE")/sqitch
+cd $(dirname "$BASH_SOURCE")/../db/sqitch
 docker run --link db:db -v `pwd`:`pwd` -w `pwd` --net $dockernetwork --rm docteurklein/sqitch:pgsql deploy db:pg://$user:$pass@$host:$port/$db
 # Verify
 docker run --link db:db -v `pwd`:`pwd` -w `pwd` --net $dockernetwork --rm docteurklein/sqitch:pgsql verify db:pg://$user:$pass@$host:$port/$db
